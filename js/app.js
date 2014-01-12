eb6a5a3bd8dfcca42df9034aac2797e01ed0982a
Tommy Palmer <hi@tommyp.org>
2014-01-12 15:10:09 +0000

    test and code for calculating the count of each coin

--- js/app.js
+++ js/app.js
@@ -38,3 +38,31 @@ var Calc = function(amount) {
     }
   ];
 }
+
+// The calculate function will take the count of each coin we need and update the coins array
+
+Calc.prototype.calculate = function() {
+  // Set the remaining amount
+  var remaining_amount = this.amount;
+  // Save the context of this, because in the _.each this becomes the window
+  var self = this;
+  // Loop over each of the coin denominations
+  _.each(this.coins, function(element, index, list) {
+    // Get the size of the coin
+    var coin = element['coin'];
+
+    // Find the remainder after dividing the remaining_amount by the coin
+    // The first time this loop runs, remaining_amount will be the amount we set originally
+    // Each time after, it will be the remainder
+    var remainder = remaining_amount % coin;
+
+    // Get the amount divisible by the current coin
+    var divisible_by_coin = remaining_amount - remainder;
+
+    // Change the remaining_amount variable to be whatever is left over
+    remaining_amount = remainder;
+
+    // Set the count of the current coin to the result of dividing the remaining_amount without the remainder, by the coin denomination
+    self.coins[index]['count'] = divisible_by_coin/coin;
+  })
+}
--- js/test/test.js
+++ js/test/test.js
@@ -1,4 +1,17 @@
+c = new Calc(123)
+
 test( "create a Calc object with an amount of 123p", function() {
-  c = new Calc(123)
   equal(c.amount, 123, "Passed!")
+});
+
+test( "calculate the amount of coins needed correctly", function() {
+  c.calculate()
+  equal(c.coins[0]['count'], 0, "Has 0 £2 coins")
+  equal(c.coins[1]['count'], 1, "Has 1 £1 coin")
+  equal(c.coins[2]['count'], 0, "Has 0 50p coins")
+  equal(c.coins[3]['count'], 1, "Has 1 20p coin")
+  equal(c.coins[4]['count'], 0, "Has 0 10p coins")
+  equal(c.coins[5]['count'], 0, "Has 0 5p coins")
+  equal(c.coins[6]['count'], 1, "Has 1 2p coin")
+  equal(c.coins[7]['count'], 1, "Has 1 1p coin")
 });
