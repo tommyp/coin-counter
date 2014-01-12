d2ac47cf8d7dbdfb89a824256b2248eb74de6f27
Tommy Palmer <hi@tommyp.org>
2014-01-12 16:24:20 +0000

    adding a view object, validation of input and an error message with tests for the view

--- js/app.js
+++ js/app.js
@@ -37,7 +37,7 @@ var Calc = function(amount) {
       count : 0
     }
   ];
-}
+};
 
 // The calculate function will take the count of each coin we need and update the coins array
 
@@ -65,4 +65,48 @@ Calc.prototype.calculate = function() {
     // Set the count of the current coin to the result of dividing the remaining_amount without the remainder, by the coin denomination
     self.coins[index]['count'] = divisible_by_coin/coin;
   })
-}
+};
+
+// Coin Counter View
+
+var CounterView = function() {
+  // Change context of this and set attributes
+  var self = this;
+  self.form = $('form');
+  self.input = $('input#amount');
+  self.calc = new Calc;
+  self.pennies = 0;
+
+  // On submission of form...
+  self.form.submit(function(ev){
+      // prevent it from doing anything
+      ev.preventDefault();
+      // Hide the error message
+      $('.error').hide();
+      // Run the sanitize input function
+      self.sanitizeInput();
+  });
+};
+
+CounterView.prototype.sanitizeInput = function() {
+  // Take the input value as a string and make sure it is formatted as a UK currency
+  if (this.input.val().match(/^£\d*\.?\d\d|\d\dp/g)) {
+    // Remove all non-numeric characters
+    var pennies = parseInt(this.input.val().replace(/\D/g,''))
+
+    // Check to make sure it is an integer
+    if (pennies % 1 == 0) {
+
+      // Set the pennies value to what is in our input
+      this.pennies = pennies;
+    };
+  } else {
+    
+    // If it doesn't format as a UK currency, show the error
+    $('.error').text("That doesn't look like a valid amount!").show();
+  };
+};
+
+$(document).ready(function() {
+  var view = new CounterView();
+});
--- js/test/test.js
+++ js/test/test.js
@@ -1,7 +1,7 @@
 c = new Calc(123)
 
 test( "create a Calc object with an amount of 123p", function() {
-  equal(c.amount, 123, "Passed!")
+  equal(c.amount, 123, "Passed")
 });
 
 test( "calculate the amount of coins needed correctly", function() {
@@ -14,4 +14,17 @@ test( "calculate the amount of coins needed correctly", function() {
   equal(c.coins[5]['count'], 0, "Has 0 5p coins")
   equal(c.coins[6]['count'], 1, "Has 1 2p coin")
   equal(c.coins[7]['count'], 1, "Has 1 1p coin")
+});
+
+view = new CounterView();
+
+test( "create a new CounterView object", function() {
+  equal(view.pennies, 0, "Passed")
+});
+
+test( "CounterView can get an input value", function() {
+  $('#amount').val('£1.34');
+  equal(view.input.val(), '£1.34', "Gets value");
+  view.sanitizeInput();
+  equal(view.pennies, 134, "Gets pennies as an integer from input string");
 });
