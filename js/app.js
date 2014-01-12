c82009a7346417746d0056fa5cc3f6ce6aca6293
Tommy Palmer <hi@tommyp.org>
2014-01-12 14:43:43 +0000

    adding coins array to calc object

--- js/app.js
+++ js/app.js
@@ -3,4 +3,38 @@
 
 var Calc = function(amount) {
   this.amount = amount;
+  this.coins = [
+    {
+      coin : 200, 
+      count : 0
+    },
+    {
+      coin : 100, 
+      count : 0
+    },
+    {
+      coin : 50, 
+      count : 0
+    },
+    {
+      coin : 20, 
+      count : 0
+    },
+    {
+      coin : 10, 
+      count : 0
+    },
+    {
+      coin : 5, 
+      count : 0
+    },
+    {
+      coin : 2, 
+      count : 0
+    },
+    {
+      coin : 1, 
+      count : 0
+    }
+  ];
 }
